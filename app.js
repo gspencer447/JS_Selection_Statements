abba6f3deede69731f1df8a778319092e9a0ae14
Graham Spencer <gspencer447@yahoo.com>
2023-05-22 13:33:47 -0500

    completed exercise 1

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
+// console.log("Hello World!\n==========\n");
+// console.log(
+//   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
+// );
+
+let faveNum = 13;
+let userAnswer = window.prompt("Pick a number");
+console.log(userAnswer);
+
+if (userAnswer < faveNum){
+    console.log("too low");
+}
+
+  else if (userAnswer > faveNum){
+    console.log("too high");
+}
+
+else {
+  console.log("Congratulations!!!");
+}
