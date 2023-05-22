ff4b1f5830997d6bfc6863f0fb1f16e3fae55060
Graham Spencer <gspencer447@yahoo.com>
2023-05-22 14:35:50 -0500

    Completed Exercise 3

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
-// console.log("Hello World!\n==========\n");
-// console.log(
-//   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-// );
+// //Exercise 1
+let faveNum = 13;
+let userAnswer = window.prompt("Pick a number");
+console.log(userAnswer);
 
-// let faveNum = 13;
-// let userAnswer = window.prompt("Pick a number");
-// console.log(userAnswer);
+if (userAnswer < faveNum){
+    console.log("too low");
+}
 
-// if (userAnswer < faveNum){
-//     console.log("too low");
-// }
+  else if (userAnswer > faveNum){
+    console.log("too high");
+}
 
-//   else if (userAnswer > faveNum){
-//     console.log("too high");
-// }
+else {
+  console.log("Congratulations!!!");
+}
 
-// else {
-//   console.log("Congratulations!!!");
-// }
-
+// //Exercise 2
 let birthMonth = window.prompt("What is your birth month?");
 console.log(birthMonth);
 switch (birthMonth){
@@ -36,3 +33,50 @@ switch (birthMonth){
   break;
   default: console.log("Your birthday is in Fall");
 }
+
+//Exercise 3
+let typeId = "01";
+let colorId = "PU";
+let sizeId = "L";
+typeId = parseInt(typeId);
+let type = "";
+let color = "";
+let size = "";
+
+switch (typeId){
+  case 01: type = "Tank top";
+  break;
+  case 02: type = "T-Shirt";
+  break;
+  case 03: type = "Long Sleeve";
+  break;
+  case 04: type = "Sweat Shirt";
+  break;
+  default: type = "Other";
+}
+
+switch (colorId){
+  case 'BK': color = "Black";
+  break;
+  case 'BL': color = "Blue";
+  break;
+  case 'RD': color = "Red";
+  break;
+  case 'PU': color = "Purple";
+  break;
+  default: color = "White";
+}
+
+switch (sizeId){
+  case 'S': size = "Small";
+  break;
+  case 'M': size = "Medium";
+  break;
+  case 'L': size = "Large";
+  break;
+  case 'XL': size = "Extra Large";
+  break;
+  default: size = "One Size Fits All";
+}
+
+console.log(`Product: ${size} ${color} ${type}`);
